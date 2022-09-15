3708eae6ccba8d21635358d11524272c0d68c905
richard2 <richard.asenciov@gmail.com>
2022-09-15 11:06:42 -0500

    Primer commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Server from "./clases/server";
+import { SERVER_PORT } from "./global/enviroments";
+import router from "./routes/router";
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+
+const server  = new Server();
+
+
+//Body Paser 
+
+server.app.use( bodyParser.urlencoded({ extended : true}))
+server.app.use( bodyParser.json())
+
+//Cors
+server.app.use( cors({ origin: true, credentials: true}))
+
+//Rutas de servicios 
+server.app.use('/', router)
+
+server.start(()=>{
+    console.log("Corriendo el servidor en el puerto", server.port)
+})
